cb809c5471a094dac6252dd3d4fc0a740fab8cee
AntonioCS <antoniocs@gmail.com>
2012-01-19 23:44:57 +0000

    Unit tests change and added new file (new version of acs_view

---
+++ libs/acs_view2.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class acs_view {
+
+    public static $PATH;
+
+    public function __construct($view, $config = null) {
+
+
+    }
+
+}
--- unitTests/test1.php
+++ unitTests/test1.php
@@ -1,28 +1,28 @@
-<?php 
+<?php
 
 if (function_exists('xdebug_disable'))
 	xdebug_disable();
 
-require('../libs/acs_view.php');
+require('../libs/acs_view2.php');
 
 
 class AcsViewTest extends PHPUnit_Framework_TestCase {
 
 	protected $v;
- 
-    protected function setUp()
-    {
+
+    protected function setUp() {
         $this->v = new acs_view();
     }
-    public function testCreatesInstance() {							
-		$this->assertNotEquals($this->v, null);		
+
+    public function testCreatesInstance() {
+		$this->assertInstanceOf('acs_view',$this->v);
     }
-	
-	public function testSetConfig() {	
-		acs_view::$PATH = 'teste';				
+
+	public function testSetConfig() {
+		//acs_view::$PATH = 'teste';
 	}
-	
+
 	public function testView() {
-		$this->v->loadview('index');
+		//$this->v->loadview('index');
 	}
 }
