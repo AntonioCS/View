f5e1731a1612b1853ddf20a45d15162042e596f2
AntonioCS <antoniocs@gmail.com>
2012-01-23 00:36:37 +0000

    Unit test updates

--- libs/acs_view2.php
+++ libs/acs_view2.php
@@ -3,11 +3,100 @@
 
 class acs_view {
 
-    public static $PATH;
+    /**
+    * Array that holds the data for the view
+    *
+    * @var array
+    */
+    private $_data = array();
 
-    public function __construct($view, $config = null) {
+    /**
+    * Configurations for this view
+    *
+    * @var array
+    */
+    private $_config = array(
+                            'ext' => 'tpl.php',
+                            'path' => 'tpls/'
+                        );
+
+    /**
+    * Bool to check if the view has been rendered
+    *
+    * @var bool
+    */
+    private $_hasRendered = false;
 
 
+    /**
+    * Path to the view
+    *
+    * @var string
+    */
+    private $_view = null;
+
+
+    /**
+    * Global path for the views/templates
+    *
+    * @var string
+    */
+    public static $PATH = 'tpls/';
+
+    /**
+    * Construct
+    *
+    * @param string|null $view  - Relative/Absolute path to the view/template
+    * @param array|null $config - Config set up for the instance
+    * @return acs_view
+    */
+    public function __construct($view = null, $config = null) {
+
+        if ($config) {
+            $this->_config = $config;
+        }
+        else {
+            $this->_config['path'] = self::$PATH;
+        }
+
+        if ($view)
+            $this->load($view);
+    }
+
+    /**
+    * Set the view to rendered
+    *
+    * @param string $view
+    */
+    public function load($view) {
+
+    }
+
+    /**
+    * Return hasRendered property
+    *
+    */
+    public function hasRendered() {
+        return $this->_hasRendered;
+    }
+
+
+    /**
+    * Return view path(s)
+    *
+    * @return string|array
+    */
+    public function getViewPath() {
+        return $this->_config['path'];
+    }
+
+    /**
+    * Set view path
+    *
+    * @param string|array $path
+    */
+    public function setViewPath($path = null) {
+        $this->_config['path'] = $path ?: self::$PATH;
     }
 
 }
--- unitTests/test1.php
+++ unitTests/test1.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+*  Unit tests for Acs View
+*/
+
 if (function_exists('xdebug_disable'))
 	xdebug_disable();
 
@@ -18,11 +22,25 @@ class AcsViewTest extends PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf('acs_view',$this->v);
     }
 
-	public function testSetConfig() {
-		//acs_view::$PATH = 'teste';
+    public function testHasPATH() {
+        $this->assertClassHasStaticAttribute('PATH', 'acs_view');
+    }
+
+	public function testSetConfigGetSetPath() {
+        $this->assertEquals('tpls/', $this->v->getViewPath());
+
+        $this->v->setViewPath('test');
+        $this->assertEquals('test', $this->v->getViewPath());
+
+        $this->v->setViewPath();
+        $this->assertEquals('tpls/', $this->v->getViewPath());
 	}
 
 	public function testView() {
 		//$this->v->loadview('index');
 	}
+
+    public function testIsRenderedFalse() {
+        $this->assertFalse($this->v->hasRendered());
+    }
 }
