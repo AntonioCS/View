7f23f8ec60f97417075095b4a3677cdd911e37f0
AntonioCS <antoniocs@gmail.com>
2012-02-22 01:15:24 +0000

    Updates

--- templates/index3.tpl.php
+++ templates/index3.tpl.php
@@ -1,4 +1,4 @@
 <?php $this->expands('index2') ?>
-<?php $this->blockStart('scripts',true,4) ?>
+<?php $this->blockStart('scripts',true) ?>
 <script type="text/javascript" src="example4.js"></script>
 <?php $this->blockEnd() ?>
---
+++ templates/templateInsideTemplate.tpl.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title><?php echo $this->title ?></title>
+</head>
+<body>
+<?php echo $this->body ?>
+<div id="footer">
+    <?php $this->load('footer')->render() ?>
+</div>
+</body>
+</html>
