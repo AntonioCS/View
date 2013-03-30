db2f2e1c1d6e0868f77b680e21ec6b74c6e7babb
antoniocs <antoniocs@gmail.com>
2013-03-30 17:57:01 +0000

    Added basic benchmark test

---
+++ tests/benchmarks/renderTest.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../../../../xhprof/external/header.php';
+require('../../src/view.php');
+
+$startTime = microtime(true);
+$view = new View\view();
+
+$view->setPath('../phpUnit/templates/');
+$view->load('index');
+
+for ($i=0;$i<10000;$i++) {
+        $view->render();
+}
+
+echo "Time:  " . number_format(( microtime(true) - $startTime), 4) . " Seconds\n";
+include_once '../../../../xhprof/external/footer.php';
